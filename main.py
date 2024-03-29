e2ae0b892831623472f04271238157f990f1dd6c
Abraham Saldivar <saldivara1010@gmail.com>
2024-03-29 15:30:01 -0500

    pls work-5

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from datetime import datetime, timedelta
 import requests
 import textwrap
+from flask import Flask, request
 from TP_lib import gt1151
 from TP_lib import epd2in13_V4 as epd2in13
 import time
 import logging
 from PIL import Image, ImageDraw, ImageFont
+import threading
+import subprocess
+import os
+
+app = Flask(__name__)
 
 # Canvas API configuration
 CANVAS_API_URL = 'https://canvas.uh.edu/api/v1'
 ACCESS_TOKEN = '23057~o8gIcEuxleDiH3vOQCQT2AxWZlceuIdRusglN94Rf1VC7X83PD3K3ppYGIGiMlay'
+# COURSE_ID = '8637'
 COURSE_IDS = ['8637', '7398', '10778', '9245']
+
+# Initialize e-ink display
+epd = epd2in13.EPD()
+gt = gt1151.GT1151()
+GT_Dev = gt1151.GT_Development()
+GT_Old = gt1151.GT_Development()
 
 logging.basicConfig(level=logging.DEBUG)
 flag_t = 1
@@ -55,85 +68,76 @@ def get_assignments(course_id):
         'Authorization': f'Bearer {ACCESS_TOKEN}',
     }
 
-    print("Fetching assignments from Canvas API...")
+    print("Fetching assignments from Canvas API for course:", course_id)
     # Fetch the list of assignments for the specified course
     response = requests.get(
         f'{CANVAS_API_URL}/courses/{course_id}/assignments', headers=headers)
 
     if response.status_code == 200:
         assignments = response.json()
+        print("Fetched assignments:", assignments)
         today = datetime.now().date()
-        tomorrow = today + timedelta(days=1)
-        day_after_tomorrow = today + timedelta(days=2)
+        next_seven_days = [today + timedelta(days=i) for i in range(7)]
         
-        print("Today:", today)
-        print("Tomorrow:", tomorrow)
-        print("Day after tomorrow:" ,day_after_tomorrow)
+        print("Next seven days:", next_seven_days)
         
-        # Filter assignments due today and for the next three days
+        # Filter assignments due today or for the next seven days
         due_assignments = [
             assignment for assignment in assignments
             if 'due_at' in assignment and assignment['due_at'] is not None
-            and (datetime.strptime(assignment['due_at'], '%Y-%m-%dT%H:%M:%SZ').date() in [today, tomorrow, day_after_tomorrow])
+            and (datetime.strptime(assignment['due_at'], '%Y-%m-%dT%H:%M:%SZ').date() in next_seven_days)
         ]
-        
-        print("Due Assigments:")
-        for assign in assignments:
-            print(f" - {assign['name']}: {assign['due_at']}")
         
         return due_assignments
     else: 
-        print("Failed to fetch assigments")
+        print("Failed to fetch assignments")
         return []
+
 
 def create_assignments_text(assignments):
     assignments_text = ''
     
     if assignments:
-        assignments_text += 'Assignments due today and for the next 3 days:\n\n'
+        assignments_text += 'Assignments due today and for the next 7 days:\n\n'
 
         for assign in assignments:
             due_date = datetime.strptime(assign['due_at'],'%Y-%m-%dT%H:%M:%SZ').date()
             
             # Format the date 
-            
             if due_date == datetime.now().date():
                 due_date_text = datetime.strptime(assign['due_at'],'%Y-%m-%dT%H:%M:%SZ').strftime('%H:%M') + " today"
-            
             else: 
                 due_date_text = due_date.strftime('%m/%d/%Y')
                 
             assignment_text = f"* {assign['name']} (Due: {due_date_text})\n"
             assignments_text += assignment_text
     else:
-        assignments_text = "No assignments available for today and for the next 3 days."
-
+        print("No assignments available.")
+        assignments_text = "No assignments available for the next 7 days."
+        
+    print("Assignments text:", assignments_text)
     return assignments_text
 
-if __name__ == "__main__":
-    epd = epd2in13.EPD()
-    gt = gt1151.GT1151()
-    GT_Dev = gt1151.GT_Development()
-    GT_Old = gt1151.GT_Development()
-    
-    t = threading.Thread(target = pthread_irq)
-    t.setDaemon(True)
-    t.start()
 
+if __name__ == "__main__":
     while True:
         all_assignments = []
 
         for course_id in COURSE_IDS:
             assignments = get_assignments(course_id)
             all_assignments.extend(assignments)
+            
+        print("All Assigments:", all_assignments)
 
         assignments_text = create_assignments_text(all_assignments)
+        
+        print("Assignments text:", assignments_text)
 
         if assignments_text:
             update_display(assignments_text)
         else:
-            update_display("No assignments available for today and for the next 3 days.")
+            update_display("No assignments available.")
 
         # Wait for 2 minutes before checking again
         print("Waiting for 2 minutes before checking again...")
-        time.sleep(120)  # Sleep for 2 minutes (120 seconds)
+        time.sleep(300)  # Sleep for 2 minutes (120 seconds)
